93876f1d18dad55f4490e97a79228dd7282df8d1
OllisGit <olaf.kaus+github@gmail.com>
2021-11-27 10:45:22 +0100

    some adjustments

--- octoprint_Playground/__init__.py
+++ octoprint_Playground/__init__.py
@@ -70,7 +70,7 @@ class PlaygroundPlugin(octoprint.plugin.SettingsPlugin,
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Playground Plugin"
-
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
     global __plugin_implementation__
